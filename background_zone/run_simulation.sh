296283098fee158195cd649b02bab7e790724bd6
alberto1000 <alberto.lolli94@gmail.com>
2024-10-30 01:21:57 +0100

    Add run_simulation.sh script and update simulation setup files

---
+++ background_zone/run_simulation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Go to background_zone directory and set up the background mesh
+cd ../background_zone
+blockMesh
+topoSet -dict system/topoSetDictR2 | tee log.topoSet
+mv log.topoSet log.topoSet1
+refineMesh -dict system/refineMeshDict2 -overwrite
+
+# Go to component_zone and set up the component mesh
+cd ../component_zone
+blockMesh
+surfaceFeatureExtract
+decomposePar
+mpirun -np 24 snappyHexMesh -parallel -overwrite
+reconstructParMesh -constant
+topoSet
+
+# Merge component and background meshes and finalize setup
+cd ../background_zone
+mergeMeshes . ../component_zone -overwrite
+checkMesh
+topoSet -dict system/topoSetDict | tee log.topoSet
+mv log.topoSet log.topoSet2
+setFields
+
+# Decompose domain and run simulation
+decomposePar
+mpirun -np 24 overPimpleDyMFoam -parallel | tee log.simulation
+
+# Reconstruct final mesh and open ParaView for visualization
+reconstructParMesh -constant
+paraFoam
